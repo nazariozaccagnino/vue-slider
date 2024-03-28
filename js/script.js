3c6cb94c80c9adabe9bb0d47e61e704c02c2214f
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-28 16:11:19 +0100

    created the vue commands

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 import {slides} from './data.js'
+
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    },
+}).mount('#app')
